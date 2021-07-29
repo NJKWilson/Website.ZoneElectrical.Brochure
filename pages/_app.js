97c94f75bf095803621a68a43d523e21340cbe80
Nathan Wilson <Nathan@XeppIT.co.uk>
2021-07-29 09:30:15 +0100

    Building out website

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,10 @@
 import 'tailwindcss/tailwind.css'
+import '@fortawesome/fontawesome-svg-core/styles.css'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+
+library.add(fab, faCoffee)
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,28 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'Zone': {
+          DEFAULT: '#9ED522',
+          '50': '#F3FAE2',
+          '100': '#EAF7CC',
+          '200': '#D7F0A0',
+          '300': '#C5E974',
+          '400': '#B3E248',
+          '500': '#9ED522',
+          '600': '#7DA91B',
+          '700': '#5D7D14',
+          '800': '#3C510D',
+          '900': '#1C2506'
+        },
+      },
+    },
   },
   variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms'),
+  ],
 }
