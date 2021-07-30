34dca6bc1eecfb3fc239d33307255492de4d9330
Nathan Wilson <Nathan@XeppIT.co.uk>
2021-07-30 08:59:51 +0100

    fixed keys problem

--- components/ourcustomers.js
+++ components/ourcustomers.js
@@ -1,10 +1,10 @@
 const features = [
-    { logo: "images/customers/dcww.png", alt: "DCWW Logo", link: "/accreditations/iso9001" },
-    { logo: "images/customers/morgan-sindall.jpg", alt: "Morgan Sindall Logo", link: "/accreditations/iso9001" },
-    { logo: "images/customers/mmb.jpg", alt: "MMB Logo", link: "/accreditations/iso9001" },
-    { logo: "images/customers/whitlandengineering.png", link: "/accreditations/iso9001" },
-    { logo: "images/customers/pbgelatins.png", alt: "PB Gelatins Logo", link: "/accreditations/iso9001" },
-    { logo: "images/customers/skanska.png", alt: "Skanska Logo", link: "/accreditations/iso9001" },
+    { key: "dcww", logo: "images/customers/dcww.png", alt: "DCWW Logo", link: "/accreditations/iso9001" },
+    { key: "morgansindall", logo: "images/customers/morgan-sindall.jpg", alt: "Morgan Sindall Logo", link: "/accreditations/iso9001" },
+    { key: "mmb", logo: "images/customers/mmb.jpg", alt: "MMB Logo", link: "/accreditations/iso9001" },
+    { key: "whitlandengineering", logo: "images/customers/whitlandengineering.png", link: "/accreditations/iso9001" },
+    { key: "pbgelatins", logo: "images/customers/pbgelatins.png", alt: "PB Gelatins Logo", link: "/accreditations/iso9001" },
+    { key: "skanska", logo: "images/customers/skanska.png", alt: "Skanska Logo", link: "/accreditations/iso9001" },
   ]
 
 export default function OurCustomers() {
@@ -20,7 +20,7 @@ export default function OurCustomers() {
           </p>
           <div className="mt-6 grid grid-cols-2 gap-0.5 md:grid-cols-3 lg:mt-8">
           {features.map((feature) => (
-                  <div className="col-span-1 flex justify-center py-8 px-8 bg-white">
+                  <div key={feature.key} className="col-span-1 flex justify-center py-8 px-8 bg-white">
                     <img
                         className="max-h-20"
                         src={feature.logo}
