487c3e5ac1c13857f9c241f74ebde6018a958eee
Nathan WIlson <njkwilson@outlook.com>
2021-09-01 16:28:47 +0100

    add app prop

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,10 +4,11 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { fab } from '@fortawesome/free-brands-svg-icons'
 import { faCoffee } from '@fortawesome/free-solid-svg-icons'
 import Layout from './layout'
+import { AppProps } from 'next/app'
 
 library.add(fab, faCoffee)
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps } : AppProps) {
   return (
     <Layout>
       <Component {...pageProps} />
